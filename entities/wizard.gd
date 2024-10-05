fc02d90ec870e6275ca3501b62650779c9612d6d
Erina <jerem.auz@gmail.com>
2024-10-05 21:37:08 +0200

    Wizard spawns its boids

--- entities/wizard.gd
+++ entities/wizard.gd
@@ -1,8 +1,19 @@
 extends Area2D
 class_name Wizard
 
+#--- Exported vars ---
+@export_category("Properties")
 @export var team = Enums.Team.RED
 @export var pool_area : Area2D
+@export var boid_node : Node2D
+
+@export_category("Prefabs")
+@export var boid_scene : PackedScene
+
+#--- private vars ---
+var my_boids = []
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,8 +21,29 @@ func _ready():
 		pool_area.add_to_group("red_pool_area")
 	if team == Enums.Team.BLUE:
 		pool_area.add_to_group("blue_pool_area")
-
+	
+	$SpawnTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func create_boid():
+	if my_boids.size() >= 50:
+		print("Too many boids. skipping creation")
+		return
+	
+	print("Creating new boid")
+	var angle = randf_range(0, 2*PI)
+	var distance = randf_range(30, 180)
+	
+	var instance: Node2D = boid_scene.instantiate()
+	var local_pos = Vector2.RIGHT.rotated(angle) * distance
+	instance.position = local_pos
+	
+	my_boids.append(instance)
+	add_child(instance)
+	instance.reparent(boid_node)
+
+func _on_spawn_timer_timeout() -> void:
+	create_boid()
